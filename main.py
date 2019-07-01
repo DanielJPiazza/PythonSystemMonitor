c5950a9bf970ce3a1eaad9dab9da039e764e2ddd
DanielJPiazza <42113905+DanielJPiazza@users.noreply.github.com>
2019-07-01 18:21:15 -0400

    Format comments for GitHub.

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from widgets import *
 			
 # MAIN
 def main():
-	root = Tk()							# Initialize tkinter
-	root.geometry("+400+200")			# Window size and position
+	root = Tk()				# Initialize tkinter
+	root.geometry("+400+200")		# Window size and position
 	root.resizable(False, False)		# Disable window resize
-	root.title("sysmon")				# Window title
+	root.title("sysmon")			# Window title
 	root.attributes("-topmost", "true")	# Defaults window to "Always On Top"
 	root.iconbitmap('monitor.ico')		# Set window icon
-	gui = GUI(root)						# Associate widgets from widgets.py
-	root.mainloop()						# Call the main loop
+	gui = GUI(root)				# Associate widgets from widgets.py
+	root.mainloop()				# Call the main loop
 	
 if __name__ == "__main__":
 	main()
