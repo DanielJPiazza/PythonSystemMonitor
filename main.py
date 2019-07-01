f6dcda02875faf3f19e184792f1dca4ea364ff66
DanielJPiazza <DanielJPiazza@users.noreply.github.com>
2019-07-01 16:46:07 -0400

    Add window icon & update .gitignore.

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ from tkinter import *
 from widgets import *
 			
 # MAIN
-root = Tk()
-root.geometry("+400+200")		# Window size and position
-root.resizable(False, False)			# Disable window resize
-root.title("sysmon")				# Window title
-root.attributes("-topmost", "true")		# Defaults window to "Always On Top"
-gui = GUI(root)					# Create GUI object
-root.mainloop()					# Call main event loop
+def main():
+	root = Tk()
+	root.geometry("+400+200")		# Window size and position
+	root.resizable(False, False)			# Disable window resize
+	root.title("sysmon")				# Window title
+	root.attributes("-topmost", "true")		# Defaults window to "Always On Top"
+	root.iconbitmap('monitor.ico')	# Set window icon
+	gui = GUI(root)					# Create GUI object
+	root.mainloop()					# Call main event loop
+	
+if __name__ == "__main__":
+	main()
