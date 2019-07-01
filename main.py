7b9504ed7f7c799a42bc6502ecca3ad72ed7aaf7
DanielJPiazza <DanielJPiazza@users.noreply.github.com>
2019-07-01 12:28:48 -0400

    Rename main source file to main.py.

---
+++ main.py
@@ -0,0 +1,12 @@
+# IMPORTS
+from tkinter import *
+from widgets import *
+			
+# MAIN
+root = Tk()
+root.geometry("220x110+400+200")		# Window size and position
+root.resizable(False, False)			# Disable window resize
+root.title("sysmon")					# Window title
+root.attributes("-topmost", "true")		# Defaults window to "Always On Top"
+gui = GUI(root)							# Create GUI object
+root.mainloop()							# Call main event loop
